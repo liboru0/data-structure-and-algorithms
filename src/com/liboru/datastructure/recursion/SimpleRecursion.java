c585799c163b78e3d49eb744a6e66a2002c6407e
liboru <1036513264@qq.com>
2021-03-17 15:27:42 +0800

    简单递归问题

---
+++ src/com/liboru/datastructure/recursion/SimpleRecursion.java
@@ -0,0 +1,24 @@
+package com.liboru.datastructure.recursion;
+
+public class SimpleRecursion {
+
+    /**
+     * @apiNote 台阶走法
+     * @param stepNum 台阶数量
+     * @author lbr
+     */
+    public int getStepThreads(int stepNum) {
+        if (stepNum == 1) {
+            return 1;
+        }
+        if (stepNum == 2) {
+            return 2;
+        }
+        return getStepThreads(stepNum - 1) + getStepThreads(stepNum - 2);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
