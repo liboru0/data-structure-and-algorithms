fa8f8432bcab640ef761bb1a2e3baba6d0f191c9
liboru <1036513264@qq.com>
2021-04-09 15:52:21 +0800

    二叉树的查找

--- src/com/liboru/datastructure/tree/BinaryTree.java
+++ src/com/liboru/datastructure/tree/BinaryTree.java
@@ -16,50 +16,125 @@ public class BinaryTree<E> {
         this.preOrderList(this.root);
     }
 
-    private void preOrderList(TreeNode<E> node){
+    private void preOrderList(TreeNode<E> node) {
         System.out.println(node.item);
-        if(node.left!=null){
+        if (node.left != null) {
             this.preOrderList(node.left);
         }
-        if(node.right!=null){
+        if (node.right != null) {
             this.preOrderList(node.right);
         }
     }
 
     // 中序遍历
-    public void infixOrderList(){
+    public void infixOrderList() {
         if (this.root == null) {
             System.out.println("二叉树为空");
         }
         this.infixOrderList(this.root);
     }
 
-    private void infixOrderList(TreeNode<E> node){
-        if(node.left!=null){
+    private void infixOrderList(TreeNode<E> node) {
+        if (node.left != null) {
             this.infixOrderList(node.left);
         }
         System.out.println(node.item);
-        if(node.right!=null){
+        if (node.right != null) {
             this.infixOrderList(node.right);
         }
     }
 
     // 后序遍历
-    public void postOrderList(){
+    public void postOrderList() {
         if (this.root == null) {
             System.out.println("二叉树为空");
         }
         this.postOrderList(this.root);
     }
 
-    private void postOrderList(TreeNode<E> node){
-        if(node.left!=null){
+    private void postOrderList(TreeNode<E> node) {
+        if (node.left != null) {
             this.postOrderList(node.left);
         }
-        if(node.right!=null){
+        if (node.right != null) {
             this.postOrderList(node.right);
         }
         System.out.println(node.item);
+    }
+
+    // 前序查找
+    public E preOrderSearch(E e) {
+        if (this.root == null) {
+            return null;
+        }
+        return this.preOrderSearch(e, this.root);
+    }
+
+    private E preOrderSearch(E e, TreeNode<E> node) {
+        if (node.item.equals(e)) {
+            return e;
+        }
+        if (node.left != null) {
+            E resultLeft = this.preOrderSearch(e, node.left);
+            if (resultLeft != null) {
+                return resultLeft;
+            }
+        }
+        if (node.right != null) {
+            return this.preOrderSearch(e, node.right);
+        }
+        return null;
+    }
+
+    // 中序查找
+    public E infixOrderSearch(E e) {
+        if (this.root == null) {
+            return null;
+        }
+        return this.infixOrderSearch(e, this.root);
+    }
+
+    private E infixOrderSearch(E e, TreeNode<E> node) {
+        if (node.left != null) {
+            E resultLeft = this.infixOrderSearch(e, node.left);
+            if (resultLeft != null) {
+                return resultLeft;
+            }
+        }
+        if (node.item.equals(e)) {
+            return e;
+        }
+        if (node.right != null) {
+            return this.infixOrderSearch(e, node.right);
+        }
+        return null;
+    }
+
+    // 后序查找
+    public E postOrderSearch(E e) {
+        if (this.root == null) {
+            return null;
+        }
+        return this.postOrderSearch(e, this.root);
+    }
+
+    private E postOrderSearch(E e, TreeNode<E> node) {
+        if (node.left != null) {
+            E resultLeft = this.postOrderSearch(e, node.left);
+            if (resultLeft != null) {
+                return resultLeft;
+            }
+        }
+        if (node.right != null) {
+            E resultRight = this.postOrderSearch(e, node.right);
+            if (resultRight != null) {
+                return resultRight;
+            }
+        }
+        if (node.item.equals(e)) {
+            return e;
+        }
+        return null;
     }
 
     public static class TreeNode<E> {
--- src/com/liboru/datastructure/tree/BinaryTreeTest.java
+++ src/com/liboru/datastructure/tree/BinaryTreeTest.java
@@ -4,9 +4,59 @@ public class BinaryTreeTest {
 
     public static void main(String[] args) {
 
-        testOrder();
+        /*testOrder();*/
+        testSearch();
 
 
+    }
+
+    /**
+     * @apiNote 测试查找
+     * @author lbr
+     */
+    public static void testSearch(){
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        BinaryTree.TreeNode<Integer> root = new BinaryTree.TreeNode<Integer>(1);
+        BinaryTree.TreeNode<Integer> node2 = new BinaryTree.TreeNode<Integer>(2);
+        BinaryTree.TreeNode<Integer> node3 = new BinaryTree.TreeNode<Integer>(3);
+        BinaryTree.TreeNode<Integer> node4 = new BinaryTree.TreeNode<Integer>(4);
+        BinaryTree.TreeNode<Integer> node5 = new BinaryTree.TreeNode<Integer>(5);
+
+        // 先手动创建二叉树
+        binaryTree.setRoot(root);
+        root.left = node2;
+        root.right = node3;
+        root.right.left = node4;
+        root.right.right = node5;
+        /**
+         *       1
+         *    2     3
+         *        4    5
+         */
+
+        System.out.println("前序查找");
+        System.out.println(binaryTree.preOrderSearch(1));
+        System.out.println(binaryTree.preOrderSearch(2));
+        System.out.println(binaryTree.preOrderSearch(3));
+        System.out.println(binaryTree.preOrderSearch(4));
+        System.out.println(binaryTree.preOrderSearch(5));
+        System.out.println(binaryTree.preOrderSearch(6));
+
+        System.out.println("中序查找");
+        System.out.println(binaryTree.infixOrderSearch(1));
+        System.out.println(binaryTree.infixOrderSearch(2));
+        System.out.println(binaryTree.infixOrderSearch(3));
+        System.out.println(binaryTree.infixOrderSearch(4));
+        System.out.println(binaryTree.infixOrderSearch(5));
+        System.out.println(binaryTree.infixOrderSearch(6));
+
+        System.out.println("后序查找");
+        System.out.println(binaryTree.postOrderSearch(1));
+        System.out.println(binaryTree.postOrderSearch(2));
+        System.out.println(binaryTree.postOrderSearch(3));
+        System.out.println(binaryTree.postOrderSearch(4));
+        System.out.println(binaryTree.postOrderSearch(5));
+        System.out.println(binaryTree.postOrderSearch(6));
     }
 
     /**
